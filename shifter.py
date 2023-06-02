0d455a10d5b4e0078506e74563ba9955637205fe
mr9733n <mr9733n@pm.me>
2023-06-03 00:02:32 +0300

    run_once param added

--- shifter.py
+++ shifter.py
@@ -18,10 +18,9 @@ DEBUG_LOG_FILE_PREFIX = "debug_"
 LOG_FILE_EXTENSION = ".log"
 LOG_FILE_NAME_FORMAT = "{}" + LOG_FILE_EXTENSION
 LOG_DEBUG_PATH = os.path.join(LOG_FOLDER, DEBUG_LOG_FILE_PREFIX +
-LOG_FILE_NAME_FORMAT.format(datetime.datetime.today().strftime("%Y-%m-%d")))
+                             LOG_FILE_NAME_FORMAT.format(datetime.datetime.today().strftime("%Y-%m-%d")))
 LOG_PATH = os.path.join(LOG_FOLDER, LOG_FILE_PREFIX +
-LOG_FILE_NAME_FORMAT.format(datetime.datetime.today().strftime("%Y-%m-%d")))
-
+                        LOG_FILE_NAME_FORMAT.format(datetime.datetime.today().strftime("%Y-%m-%d")))
 
 
 def read_config():
@@ -40,17 +39,14 @@ def read_config():
 
 
 def write_to_log(log_path, messages):
-    """Writed log messages to file"""
-    # print(write_to_log.__doc__)
+    """Writes log messages to file"""
     with open(log_path, "a") as f:
         for message in messages:
-            f.write(
-                f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {message}\n")
+            f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {message}\n")
 
 
 def write_to_debug_log(log_path, messages):
-    """Writed log messages to debug file"""
-    # print(write_to_debug_log.__doc__)
+    """Writes log messages to debug file"""
     with open(log_path, "a") as f:
         for message in messages:
             f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {message}\n")
@@ -63,17 +59,18 @@ def copy_files_job():
 
 
 def run_copy_job():
-    """Scheduled the copy job to run at a specific time"""
+    """Schedule the copy job to run at a specific time"""
     config = read_config()
     scheduled_every_15_minutes = config.get("scheduled_every_15_minutes")
     scheduled_every_hour = config.get("scheduled_every_hour")
     scheduled_time_str = config.get("scheduled_on_time")
+    run_once = config.get("run_once", False)  # Get the "run_once" parameter from config, default to False
     print(scheduled_every_15_minutes)
     print(scheduled_every_hour)
     print(scheduled_time_str)
     if scheduled_every_15_minutes:
         schedule.every(15).minutes.at(':00').do(copy_files_job)
-        write_to_debug_log(LOG_DEBUG_PATH, [f"Copy job scheduled to run 15 minutes"])
+        write_to_debug_log(LOG_DEBUG_PATH, [f"Copy job scheduled to run every 15 minutes"])
 
     if scheduled_time_str:
         try:
@@ -86,22 +83,26 @@ def run_copy_job():
                 print("Invalid scheduled time format in config file. Using default schedule.")
         except ValueError:
             print("Invalid scheduled time format in config file. Using default schedule.")
-    
+
     if scheduled_every_hour:
         # Use default schedule if no scheduled time is specified in config file
         schedule.every(1).hour.at(':00').do(copy_files_job)
         write_to_debug_log(LOG_DEBUG_PATH, [f"Copy job scheduled to run every hour"])
 
-    while True:
-        pending_jobs = schedule.jobs
-        print(f"{len(pending_jobs)} jobs pending. Waiting...")
-        schedule.run_pending()
-        time.sleep(1)
-        
+    if run_once:
+        # Run the copy job once
+        copy_files_job()
+        sys.exit()  # Exit the script after execution
+    else:
+        while True:
+            pending_jobs = schedule.jobs
+            print(f"{len(pending_jobs)} jobs pending. Waiting...")
+            schedule.run_pending()
+            time.sleep(1)
+
 
 def check_and_create_folder(folder_path, log_messages):
-    """Checked if folder exists, create it if it doesn't"""
-    # print(check_and_create_folder.__doc__)
+    """Check if folder exists, create it if it doesn't"""
     print("folder_path", folder_path)
 
     if not os.path.exists(folder_path):
@@ -113,7 +114,6 @@ def check_and_create_folder(folder_path, log_messages):
 
 
 def get_files(source_dir, ext, pattern=None):
-
     files = []
     print("source_dir: ", source_dir)
     print("ext: ", ext)
@@ -125,7 +125,7 @@ def get_files(source_dir, ext, pattern=None):
 
 
 def copy_files_in_folders():
-    """Copies files from source folders to destination folders"""
+    """Copy files from source folders to destination folders"""
     config = read_config()
     dest_folders = config["destination_folders"]
     log_messages = []
@@ -134,8 +134,8 @@ def copy_files_in_folders():
     # Get source folders from config
     source_folders = [os.path.expandvars(folder) for folder in config["source_folders"]]
 
-    print("dest_folders: ",dest_folders)
-    print("source_folders: ",source_folders)
+    print("dest_folders: ", dest_folders)
+    print("source_folders: ", source_folders)
 
     for source_folder in source_folders:
         if not os.path.exists(source_folder):
@@ -159,7 +159,7 @@ def copy_files_in_folders():
 
             # Get a list of files in the destination folder
             dest_files = set(os.listdir(dest_dir))
-            print("dest_files: ",dest_files)
+            print("dest_files: ", dest_files)
 
             # Copy files that haven't been copied already
             count = 0
@@ -174,10 +174,12 @@ def copy_files_in_folders():
                 dest_path = os.path.join(dest_dir, file_name)
                 shutil.copy2(file_path, dest_path)
                 count += 1
-                print("file_name: ",file_name)
-                print("file_path: ",file_path)
+                print("file_name: ", file_name)
+                print("file_path: ", file_path)
+
                 # Add the name of the copied file to the set of destination files
                 dest_files.add(file_name)
+
                 # If delete_after_copy is True, delete the original file
                 if delete_after_copy:
                     try:
@@ -185,19 +187,18 @@ def copy_files_in_folders():
                         write_to_debug_log(LOG_DEBUG_PATH, [f"Deleted {file_name} from {source_folder}\n"])
                     except OSError as e:
                         log_messages.append(f"Error deleting {file_name} from {source_folder}: {e}")
-                        
+
             total_count += count
             log_messages.append(f"Copied {count} files from {source_folder} to {dest_dir}")
 
             # Write filenames to debug log
             write_to_debug_log(LOG_DEBUG_PATH, [f"Copied {file_name} from {source_folder} to {dest_dir}\n"])
 
-
-    # write all log messages to file
+    # Write all log messages to file
     write_to_log(LOG_PATH, log_messages)
     write_to_debug_log(LOG_DEBUG_PATH, log_messages)
 
-    # write total count to log
+    # Write total count to log
     print(f"Total files copied: {total_count}")
     write_to_log(LOG_PATH, [f"Total files copied: {total_count}"])
     write_to_debug_log(LOG_DEBUG_PATH, [f"Total files copied: {total_count}"])
@@ -206,16 +207,14 @@ def copy_files_in_folders():
 if __name__ == '__main__':
     script_path = Path(sys.argv[0]).resolve()
     os.chdir(script_path.parent)
-    while True:
-        try:
-            print("Started...")
-            logs_dir = Path.cwd() / "Logs"
-            logs_dir.mkdir(parents=True, exist_ok=True)
-            write_to_log(LOG_PATH, [f"Service started..."])
-            write_to_debug_log(LOG_DEBUG_PATH, [f"Service started..."])
-            copy_files_in_folders()
-            run_copy_job()
+    try:
+        print("Started...")
+        logs_dir = Path.cwd() / "Logs"
+        logs_dir.mkdir(parents=True, exist_ok=True)
+        write_to_log(LOG_PATH, [f"Service started..."])
+        write_to_debug_log(LOG_DEBUG_PATH, [f"Service started..."])
+        run_copy_job()
 
-        except:
-            print("Stopping...")
-            sys.exit()
+    except:
+        print("Stopping...")
+        sys.exit()
